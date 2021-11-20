35c1a0fd59bd5d48218481c16ce1176077e9eee5
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-11-21 00:09:16 +0200

    add modal

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -5,6 +5,7 @@ import CloseIcon from '@mui/icons-material/Close';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import Container from '@mui/material/Container';
 import DatePicker from '@mui/lab/DatePicker';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
@@ -15,11 +16,14 @@ import FormControl from '@mui/material/FormControl';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+// import AddIcon from '@mui/icons-material/Add';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 export default function RegisterPage() {
   const [name, setName] = useState('');
   const [pass, setPass] = useState('');
   const [showPass, setShowPass] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [date, setDate] = useState(null);
 
   const handleChange = e => {
@@ -31,7 +35,6 @@ export default function RegisterPage() {
       case 'pass':
         setPass(value);
         break;
-        break;
       default:
         return;
     }
@@ -39,7 +42,11 @@ export default function RegisterPage() {
 
   const handleSubmit = event => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
+    //post
+  };
+
+  const handleToggleModal = () => {
+    setShowModal(!showModal);
   };
 
   const handleClickShowPassword = () => {
@@ -51,113 +58,143 @@ export default function RegisterPage() {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          textAlign: 'center',
-          border: '1px solid',
-          padding: '50px',
-          borderRadius: '40px',
-          position: 'relative',
-        }}
+    <>
+      <Button
+        onClick={handleToggleModal}
+        variant="contained"
+        size="large"
+        endIcon={<AddCircleIcon />}
       >
-        <Typography component="h1" variant="h5">
-          Create your own room
-        </Typography>
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          noValidate
+        Create a room
+      </Button>
+      <Modal
+        open={showModal}
+        onClose={handleToggleModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Container
+          component="main"
+          maxWidth="xs"
           sx={{
-            mt: 1,
+            width: '100%',
+            height: '100%',
             display: 'flex',
-            flexDirection: 'column',
+            // flexDirection: 'column',
             alignItems: 'center',
             justifyContent: 'center',
           }}
         >
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="name"
-            label="Name"
-            name="name"
-            value={name}
-            autoFocus
-            onChange={handleChange}
-          />
-
-          <FormControl sx={{ m: 1 }} variant="outlined" fullWidth>
-            <InputLabel htmlFor="outlined-adornment-password" required>
-              Password
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={showPass ? 'text' : 'password'}
-              name="pass"
-              value={pass}
-              onChange={handleChange}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPass ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              label="Password"
-            />
-          </FormControl>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <DatePicker
-              label="Party date"
-              // name="date"
-              value={date}
-              onChange={newDate => {
-                setDate(newDate);
+          <Box
+            sx={{
+              backgroundColor: 'white',
+              // marginTop: 8,
+              textAlign: 'center',
+              border: '1px solid',
+              padding: '50px',
+              borderRadius: '40px',
+              position: 'relative',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Create your own room
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{
+                mt: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
-              minDate={Date.now()}
-              renderInput={params => (
-                <TextField {...params} margin="normal" fullWidth required />
-              )}
-            />
-          </LocalizationProvider>
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                value={name}
+                autoFocus
+                onChange={handleChange}
+              />
 
-          <Button
-            type="submit"
-            // fullWidth
-            variant="contained"
-            size="large"
-            sx={{
-              margin: '0 auto',
-              mt: 3,
-              mb: 2,
-              // backgroundColor: 'green',
-            }}
-          >
-            Create a room
-          </Button>
-          <IconButton
-            aria-label="close"
-            sx={{
-              position: 'absolute',
-              top: '10px',
-              right: '10px',
-              // p: 2,
-              // minWidth: 300,
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-        </Box>
-      </Box>
-    </Container>
+              <FormControl margin="normal" variant="outlined" fullWidth>
+                <InputLabel htmlFor="outlined-adornment-password" required>
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPass ? 'text' : 'password'}
+                  name="pass"
+                  value={pass}
+                  onChange={handleChange}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPass ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  label="Party date"
+                  // name="date"
+                  value={date}
+                  onChange={newDate => {
+                    setDate(newDate);
+                  }}
+                  minDate={Date.now()}
+                  renderInput={params => (
+                    <TextField {...params} margin="normal" fullWidth required />
+                  )}
+                />
+              </LocalizationProvider>
+
+              <Button
+                type="submit"
+                // fullWidth
+                variant="contained"
+                size="large"
+                sx={{
+                  margin: '0 auto',
+                  mt: 3,
+                  mb: 2,
+                  // backgroundColor: 'green',
+                }}
+              >
+                Create a room
+              </Button>
+              <IconButton
+                aria-label="close"
+                onClick={handleToggleModal}
+                sx={{
+                  position: 'absolute',
+                  top: '10px',
+                  right: '10px',
+                  // p: 2,
+                  // minWidth: 300,
+                }}
+              >
+                <CloseIcon />
+              </IconButton>
+            </Box>
+          </Box>
+        </Container>
+      </Modal>
+    </>
   );
 }
 
